d7ab6ec294b895dd6d24584aeb46d7efe3e22a7c
krishansingh1 <kkrishan7074@gmail.com>
2021-08-17 21:14:19 +0530

    Modified

--- github.js
+++ github.js
@@ -1,18 +1,18 @@
 class GitHub{
 
     constructor(){
-        // this.client_id ='ea0545a21a0704030293';
-        // this.client_secret = '43e644c650283ef99fee660cdf2fa6017c31b0b7';
+        this.client_id ='b1976e78b3f273653547';
+        this.client_secret = 'e949b9cd15f9e0666bbbd0ed08d6cb4d5eae5e07';
         this.repos_count= 5;
         this.repos_sort = 'created: asc';
     }
 
     async getUser(user){
 
-    const profileResponse = await fetch(`https://api.github.com/users/${user}`);
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
 
-    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}`);
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
      const profile = await profileResponse.json();
      const repos = await repoResponse.json();
